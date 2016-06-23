ab4555baa4db4b20b36baabc4c2bdf7de14b62c1
llukyanov <leonid@lytit.com>
2016-06-23 00:55:55 -0400

    dependency tweak

--- lib/dependencies.rb
+++ lib/dependencies.rb
@@ -3,6 +3,6 @@ require "instascraper/version"
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
 require "capybara"
 require "capybara/dsl"
-require "capybara/poltergeist"
+#require "capybara/poltergeist"
 require "phantomjs"
 require "init/poltergeist"
